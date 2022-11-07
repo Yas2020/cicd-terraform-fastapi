c3e7e711602757519714cf536a4763482e31d890
Yas2020 <yas.eftekhari@gmail.com>
2022-11-07 18:42:16 -0500

    database conf added

--- test/database.py
+++ test/database.py
@@ -9,7 +9,7 @@ from app.models import Base
 
 
 # we need a new database instace for our testing environment called fastapi_test
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
